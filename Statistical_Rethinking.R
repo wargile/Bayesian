8c1724b6121e6ac62ededa6453d44ced684ce171
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2016-05-04 20:04:50 -0500

    update
    edited and deleted files

--- Statistical_Rethinking.R
+++ Statistical_Rethinking.R
@@ -1,37 +1,16 @@
 ## R code 0.1
-print( "All models are wrong, but some are useful." )
 
 ## R code 0.2
-x <- 1:2
-x <- x*10
-x <- log(x)
-x <- sum(x)
-x <- exp(x)
-x
+
 
 ## R code 0.3
-( log( 0.01^200 ) )
-( 200 * log(0.01) )
+
 
 ## R code 0.4
-# Load the data:
-# car braking distances in feet paired with speeds in km/h
-# see ?cars for details
-data(cars)
 
-# fit a linear regression of distance on speed
-m <- lm( dist ~ speed , data=cars )
-
-# estimated coefficients from the model
-coef(m)
-
-# plot residuals against speed
-plot( resid(m) ~ speed , data=cars )
 
 ## R code 0.5
-install.packages(c("coda","mvtnorm","devtools"))
-library(devtools)
-devtools::install_github("rmcelreath/rethinking")
+
 
 ## R code 2.1
 ways <- c( 0 , 3 , 8 , 9 , 0 )
