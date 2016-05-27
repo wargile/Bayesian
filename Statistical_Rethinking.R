736aaa96973483048e228f9b91259d8de1d7ae3a
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2016-05-26 20:41:35 -0500

    update
    edited files

--- Statistical_Rethinking.R
+++ Statistical_Rethinking.R
@@ -9,178 +9,78 @@
 ## R code 0.5
 
 ## R code 2.1
-ways <- c( 0 , 3 , 8 , 9 , 0 )
-ways/sum(ways)
 
 ## R code 2.2
-dbinom( 6 , size=9 , prob=0.5 )
 
 ## R code 2.3
-# define grid
-p_grid <- seq( from=0 , to=1 , length.out=20 )
-
-# define prior
-prior <- rep( 1 , 20 )
-
-# compute likelihood at each value in grid
-likelihood <- dbinom( 6 , size=9 , prob=p_grid )
-
-# compute product of likelihood and prior
-unstd.posterior <- likelihood * prior
-
-# standardize the posterior, so it sums to 1
-posterior <- unstd.posterior / sum(unstd.posterior)
 
 ## R code 2.4
-plot( p_grid , posterior , type="b" ,
-    xlab="probability of water" , ylab="posterior probability" )
-mtext( "20 points" )
 
 ## R code 2.5
-prior <- ifelse( p_grid < 0.5 , 0 , 1 )
-prior <- exp( -5*abs( p_grid - 0.5 ) )
 
 ## R code 2.6
-library(rethinking)
-globe.qa <- map(
-    alist(
-        w ~ dbinom(9,p) ,  # binomial likelihood
-        p ~ dunif(0,1)     # uniform prior
-    ) ,
-    data=list(w=6) )
-
-# display summary of quadratic approximation
-precis( globe.qa )
 
 ## R code 2.7
-# analytical calculation
-w <- 6
-n <- 9
-curve( dbeta( x , w+1 , n-w+1 ) , from=0 , to=1 )
-# quadratic approximation
-curve( dnorm( x , 0.67 , 0.16 ) , lty=2 , add=TRUE )
 
 ## R code 3.1
-PrPV <- 0.95
-PrPM <- 0.01
-PrV <- 0.001
-PrP <- PrPV*PrV + PrPM*(1-PrV)
-( PrVP <- PrPV*PrV / PrP )
 
 ## R code 3.2
-p_grid <- seq( from=0 , to=1 , length.out=1000 )
-prior <- rep( 1 , 1000 )
-likelihood <- dbinom( 6 , size=9 , prob=p_grid )
-posterior <- likelihood * prior
-posterior <- posterior / sum(posterior)
 
 ## R code 3.3
-samples <- sample( p_grid , prob=posterior , size=1e4 , replace=TRUE )
 
 ## R code 3.4
-plot( samples )
 
 ## R code 3.5
-library(rethinking)
-dens( samples )
 
 ## R code 3.6
-# add up posterior probability where p < 0.5
-sum( posterior[ p_grid < 0.5 ] )
 
 ## R code 3.7
-sum( samples < 0.5 ) / 1e4
 
 ## R code 3.8
-sum( samples > 0.5 & samples < 0.75 ) / 1e4
 
 ## R code 3.9
-quantile( samples , 0.8 )
 
 ## R code 3.10
-quantile( samples , c( 0.1 , 0.9 ) )
 
 ## R code 3.11
-p_grid <- seq( from=0 , to=1 , length.out=1000 )
-prior <- rep(1,1000)
-likelihood <- dbinom( 3 , size=3 , prob=p_grid )
-posterior <- likelihood * prior
-posterior <- posterior / sum(posterior)
-samples <- sample( p_grid , size=1e4 , replace=TRUE , prob=posterior )
 
 ## R code 3.12
-PI( samples , prob=0.5 )
 
 ## R code 3.13
-HPDI( samples , prob=0.5 )
 
 ## R code 3.14
-p_grid[ which.max(posterior) ]
 
 ## R code 3.15
-chainmode( samples , adj=0.01 )
 
 ## R code 3.16
-mean( samples )
-median( samples )
 
 ## R code 3.17
-sum( posterior*abs( 0.5 - p_grid ) )
 
 ## R code 3.18
-loss <- sapply( p_grid , function(d) sum( posterior*abs( d - p_grid ) ) )
 
 ## R code 3.19
-p_grid[ which.min(loss) ]
 
 ## R code 3.20
-dbinom( 0:2 , size=2 , prob=0.7 )
 
 ## R code 3.21
-rbinom( 1 , size=2 , prob=0.7 )
 
 ## R code 3.22
-rbinom( 10 , size=2 , prob=0.7 )
 
 ## R code 3.23
-dummy_w <- rbinom( 1e5 , size=2 , prob=0.7 )
-table(dummy_w)/1e5
 
 ## R code 3.24
-dummy_w <- rbinom( 1e5 , size=9 , prob=0.7 )
-simplehist( dummy_w , xlab="dummy water count" )
 
 ## R code 3.25
-w <- rbinom( 1e4 , size=9 , prob=0.6 )
 
 ## R code 3.26
-w <- rbinom( 1e4 , size=9 , prob=samples )
 
 ## R code 3.27
-p_grid <- seq( from=0 , to=1 , length.out=1000 )
-prior <- rep( 1 , 1000 )
-likelihood <- dbinom( 6 , size=9 , prob=p_grid )
-posterior <- likelihood * prior
-posterior <- posterior / sum(posterior)
-set.seed(100)
-samples <- sample( p_grid , prob=posterior , size=1e4 , replace=TRUE )
 
 ## R code 3.28
-birth1 <- c(1,0,0,0,1,1,0,1,0,1,0,0,1,1,0,1,1,0,0,0,1,0,0,0,1,0,
-0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,1,1,0,1,0,0,0,0,0,0,0,
-1,1,0,1,0,0,1,0,0,0,1,0,0,1,1,1,1,0,1,0,1,1,1,1,1,0,0,1,0,1,1,0,
-1,0,1,1,1,0,1,1,1,1)
-birth2 <- c(0,1,0,1,0,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,
-1,1,1,0,1,1,1,0,1,0,0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,
-1,1,1,0,1,1,0,1,1,0,1,1,1,0,0,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0,1,1,
-0,0,0,1,1,1,0,0,0,0)
 
 ## R code 3.29
-library(rethinking)
-data(homeworkch3)
 
 ## R code 3.30
-sum(birth1) + sum(birth2)
 
 ## R code 4.1
 pos <- replicate( 1000 , sum( runif(16,-1,1) ) )
