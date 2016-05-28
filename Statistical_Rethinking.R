bb8e7f1ff1ee6bd79a8dca05fc72f75bab2b4c6d
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2016-05-28 17:36:21 -0500

    update
    edits

--- Statistical_Rethinking.R
+++ Statistical_Rethinking.R
@@ -555,555 +555,130 @@
 ## R code 9.13
 
 ## R code 10.1
-library(rethinking)
-data(chimpanzees)
-d <- chimpanzees
 
 ## R code 10.2
-m10.1 <- map(
-    alist(
-        pulled_left ~ dbinom( 1 , p ) ,
-        logit(p) <- a ,
-        a ~ dnorm(0,10)
-    ) ,
-    data=d )
-precis(m10.1)
 
 ## R code 10.3
-logistic( c(0.18,0.46) )
 
 ## R code 10.4
-m10.2 <- map(
-    alist(
-        pulled_left ~ dbinom( 1 , p ) ,
-        logit(p) <- a + bp*prosoc_left ,
-        a ~ dnorm(0,10) ,
-        bp ~ dnorm(0,10)
-    ) ,
-    data=d )
-m10.3 <- map(
-    alist(
-        pulled_left ~ dbinom( 1 , p ) ,
-        logit(p) <- a + (bp + bpC*condition)*prosoc_left ,
-        a ~ dnorm(0,10) ,
-        bp ~ dnorm(0,10) ,
-        bpC ~ dnorm(0,10)
-    ) ,
-    data=d )
 
 ## R code 10.5
-compare( m10.1 , m10.2 , m10.3 )
 
 ## R code 10.6
-precis(m10.3)
 
 ## R code 10.7
-exp(0.61)
 
 ## R code 10.8
-logistic( 4 )
 
 ## R code 10.9
-logistic( 4 + 0.61 )
 
 ## R code 10.10
-# dummy data for predictions across treatments
-d.pred <- data.frame(
-    prosoc_left = c(0,1,0,1),   # right/left/right/left
-    condition = c(0,0,1,1)      # control/control/partner/partner
-)
-
-# build prediction ensemble
-chimp.ensemble <- ensemble( m10.1 , m10.2 , m10.3 , data=d.pred )
-
-# summarize
-pred.p <- apply( chimp.ensemble$link , 2 , mean )
-pred.p.PI <- apply( chimp.ensemble$link , 2 , PI )
 
 ## R code 10.11
-# empty plot frame with good axes
-plot( 0 , 0 , type="n" , xlab="prosoc_left/condition" ,
-    ylab="proportion pulled left" , ylim=c(0,1) , xaxt="n" ,
-    xlim=c(1,4) )
-axis( 1 , at=1:4 , labels=c("0/0","1/0","0/1","1/1") )
-
-# plot raw data, one trend for each of 7 individual chimpanzees
-# will use by() here; see Overthinking box for explanation
-p <- by( d$pulled_left ,
-    list(d$prosoc_left,d$condition,d$actor) , mean )
-for ( chimp in 1:7 )
-    lines( 1:4 , as.vector(p[,,chimp]) , col=rangi2 , lwd=1.5 )
-
-# now superimpose posterior predictions
-lines( 1:4 , pred.p )
-shade( pred.p.PI , 1:4 )
 
 ## R code 10.12
-# clean NAs from the data
-d2 <- d
-d2$recipient <- NULL
-
-# re-use map fit to get the formula
-m10.3stan <- map2stan( m10.3 , data=d2 , iter=1e4 , warmup=1000 )
-precis(m10.3stan)
 
 ## R code 10.13
-pairs(m10.3stan)
 
 ## R code 10.14
-m10.4 <- map2stan(
-    alist(
-        pulled_left ~ dbinom( 1 , p ) ,
-        logit(p) <- a[actor] + (bp + bpC*condition)*prosoc_left ,
-        a[actor] ~ dnorm(0,10),
-        bp ~ dnorm(0,10),
-        bpC ~ dnorm(0,10)
-    ) ,
-    data=d2 , chains=2 , iter=2500 , warmup=500 )
 
 ## R code 10.15
-unique( d$actor )
 
 ## R code 10.16
-precis( m10.4 , depth=2 )
 
 ## R code 10.17
-post <- extract.samples( m10.4 )
-str( post )
 
 ## R code 10.18
-dens( post$a[,2] )
 
 ## R code 10.19
-chimp <- 1
-d.pred <- list(
-    pulled_left = rep( 0 , 4 ), # empty outcome
-    prosoc_left = c(0,1,0,1),   # right/left/right/left
-    condition = c(0,0,1,1),     # control/control/partner/partner
-    actor = rep(chimp,4)
-)
-link.m10.4 <- link( m10.4 , data=d.pred )
-pred.p <- apply( link.m10.4 , 2 , mean )
-pred.p.PI <- apply( link.m10.4 , 2 , PI )
-
-plot( 0 , 0 , type="n" , xlab="prosoc_left/condition" ,
-    ylab="proportion pulled left" , ylim=c(0,1) , xaxt="n" ,
-    xlim=c(1,4) , yaxp=c(0,1,2) )
-axis( 1 , at=1:4 , labels=c("0/0","1/0","0/1","1/1") )
-mtext( paste( "actor" , chimp ) )
-
-p <- by( d$pulled_left ,
-    list(d$prosoc_left,d$condition,d$actor) , mean )
-lines( 1:4 , as.vector(p[,,chimp]) , col=rangi2 , lwd=2 )
-
-lines( 1:4 , pred.p )
-shade( pred.p.PI , 1:4 )
 
 ## R code 10.20
-data(chimpanzees)
-d <- chimpanzees
-d.aggregated <- aggregate( d$pulled_left ,
-    list(prosoc_left=d$prosoc_left,condition=d$condition,actor=d$actor) ,
-    sum )
 
 ## R code 10.21
-m10.5 <- map(
-    alist(
-        x ~ dbinom( 18 , p ) ,
-        logit(p) <- a + (bp + bpC*condition)*prosoc_left ,
-        a ~ dnorm(0,10) ,
-        bp ~ dnorm(0,10) ,
-        bpC ~ dnorm(0,10)
-    ) ,
-    data=d.aggregated )
 
 ## R code 10.22
-library(rethinking)
-data(UCBadmit)
-d <- UCBadmit
 
 ## R code 10.23
-d$male <- ifelse( d$applicant.gender=="male" , 1 , 0 )
-m10.6 <- map(
-    alist(
-         admit ~ dbinom( applications , p ) ,
-         logit(p) <- a + bm*male ,
-         a ~ dnorm(0,10) ,
-         bm ~ dnorm(0,10)
-    ) ,
-    data=d )
-m10.7 <- map(
-    alist(
-         admit ~ dbinom( applications , p ) ,
-         logit(p) <- a ,
-         a ~ dnorm(0,10)
-    ) ,
-    data=d )
 
 ## R code 10.24
-compare( m10.6 , m10.7 )
 
 ## R code 10.25
-precis(m10.6)
 
 ## R code 10.26
-post <- extract.samples( m10.6 )
-p.admit.male <- logistic( post$a + post$bm )
-p.admit.female <- logistic( post$a )
-diff.admit <- p.admit.male - p.admit.female
-quantile( diff.admit , c(0.025,0.5,0.975) )
 
 ## R code 10.27
-postcheck( m10.6 , n=1e4 )
-# draw lines connecting points from same dept
-for ( i in 1:6 ) {
-    x <- 1 + 2*(i-1)
-    y1 <- d$admit[x]/d$applications[x]
-    y2 <- d$admit[x+1]/d$applications[x+1]
-    lines( c(x,x+1) , c(y1,y2) , col=rangi2 , lwd=2 )
-    text( x+0.5 , (y1+y2)/2 + 0.05 , d$dept[x] , cex=0.8 , col=rangi2 )
-}
 
 ## R code 10.28
-# make index
-d$dept_id <- coerce_index( d$dept )
-
-# model with unique intercept for each dept
-m10.8 <- map(
-    alist(
-        admit ~ dbinom( applications , p ) ,
-        logit(p) <- a[dept_id] ,
-        a[dept_id] ~ dnorm(0,10)
-    ) , data=d )
-
-# model with male difference as well
-m10.9 <- map(
-    alist(
-        admit ~ dbinom( applications , p ) ,
-        logit(p) <- a[dept_id] + bm*male ,
-        a[dept_id] ~ dnorm(0,10) ,
-        bm ~ dnorm(0,10)
-    ) , data=d )
 
 ## R code 10.29
-compare( m10.6 , m10.7 , m10.8 , m10.9 )
 
 ## R code 10.30
-precis( m10.9 , depth=2 )
 
 ## R code 10.31
-m10.9stan <- map2stan( m10.9 , chains=2 , iter=2500 , warmup=500 )
-precis(m10.9stan,depth=2)
 
 ## R code 10.32
-m10.7glm <- glm( cbind(admit,reject) ~ 1 , data=d , family=binomial )
-m10.6glm <- glm( cbind(admit,reject) ~ male , data=d , family=binomial )
-m10.8glm <- glm( cbind(admit,reject) ~ dept , data=d , family=binomial )
-m10.9glm <- glm( cbind(admit,reject) ~ male + dept , data=d ,
-    family=binomial )
 
 ## R code 10.33
-data(chimpanzees)
-m10.4glm <- glm(
-    pulled_left ~ as.factor(actor) + prosoc_left * condition - condition ,
-    data=chimpanzees , family=binomial )
 
 ## R code 10.34
-glimmer( pulled_left ~ prosoc_left * condition - condition ,
-    data=chimpanzees , family=binomial )
 
 ## R code 10.35
-# outcome and predictor almost perfectly associated
-y <- c( rep(0,10) , rep(1,10) )
-x <- c( rep(-1,9) , rep(1,11) )
-# fit binomial GLM
-m.bad <- glm( y ~ x , data=list(y=y,x=x) , family=binomial )
-precis(m.bad)
 
 ## R code 10.36
-m.good <- map(
-    alist(
-        y ~ dbinom( 1 , p ),
-        logit(p) <- a + b*x,
-        c(a,b) ~ dnorm(0,10)
-    ) , data=list(y=y,x=x) )
-precis(m.good)
 
 ## R code 10.37
-m.good.stan <- map2stan( m.good )
-pairs(m.good.stan)
 
 ## R code 10.38
-y <- rbinom(1e5,1000,1/1000)
-c( mean(y) , var(y) )
 
 ## R code 10.39
-library(rethinking)
-data(Kline)
-d <- Kline
-d
 
 ## R code 10.40
-d$log_pop <- log(d$population)
-d$contact_high <- ifelse( d$contact=="high" , 1 , 0 )
 
 ## R code 10.41
-m10.10 <- map(
-    alist(
-        total_tools ~ dpois( lambda ),
-        log(lambda) <- a + bp*log_pop +
-            bc*contact_high + bpc*contact_high*log_pop,
-        a ~ dnorm(0,100),
-        c(bp,bc,bpc) ~ dnorm(0,1)
-    ),
-    data=d )
 
 ## R code 10.42
-precis(m10.10,corr=TRUE)
-plot(precis(m10.10))
 
 ## R code 10.43
-post <- extract.samples(m10.10)
-lambda_high <- exp( post$a + post$bc + (post$bp + post$bpc)*8 )
-lambda_low <- exp( post$a + post$bp*8 )
 
 ## R code 10.44
-diff <- lambda_high - lambda_low
-sum(diff > 0)/length(diff)
 
 ## R code 10.45
-# no interaction
-m10.11 <- map(
-    alist(
-        total_tools ~ dpois( lambda ),
-        log(lambda) <- a + bp*log_pop + bc*contact_high,
-        a ~ dnorm(0,100),
-        c(bp,bc) ~ dnorm( 0 , 1 )
-    ), data=d )
 
 ## R code 10.46
-# no contact rate
-m10.12 <- map(
-    alist(
-        total_tools ~ dpois( lambda ),
-        log(lambda) <- a + bp*log_pop,
-        a ~ dnorm(0,100),
-        bp ~ dnorm( 0 , 1 )
-    ), data=d )
-
-# no log-population
-m10.13 <- map(
-    alist(
-        total_tools ~ dpois( lambda ),
-        log(lambda) <- a + bc*contact_high,
-        a ~ dnorm(0,100),
-        bc ~ dnorm( 0 , 1 )
-    ), data=d )
 
 ## R code 10.47
-# intercept only
-m10.14 <- map(
-    alist(
-        total_tools ~ dpois( lambda ),
-        log(lambda) <- a,
-        a ~ dnorm(0,100)
-    ), data=d )
-
-# compare all using WAIC
-# adding n=1e4 for more stable WAIC estimates
-# will also plot the comparison
-( islands.compare <- compare(m10.10,m10.11,m10.12,m10.13,m10.14,n=1e4) )
-plot(islands.compare)
 
 ## R code 10.48
-# make plot of raw data to begin
-# point character (pch) indicates contact rate
-pch <- ifelse( d$contact_high==1 , 16 , 1 )
-plot( d$log_pop , d$total_tools , col=rangi2 , pch=pch ,
-    xlab="log-population" , ylab="total tools" )
-
-# sequence of log-population sizes to compute over
-log_pop.seq <- seq( from=6 , to=13 , length.out=30 )
-
-# compute trend for high contact islands
-d.pred <- data.frame(
-    log_pop = log_pop.seq,
-    contact_high = 1
-)
-lambda.pred.h <- ensemble( m10.10 , m10.11 , m10.12 , data=d.pred )
-lambda.med <- apply( lambda.pred.h$link , 2 , median )
-lambda.PI <- apply( lambda.pred.h$link , 2 , PI )
-
-# plot predicted trend for high contact islands
-lines( log_pop.seq , lambda.med , col=rangi2 )
-shade( lambda.PI , log_pop.seq , col=col.alpha(rangi2,0.2) )
-
-# compute trend for low contact islands
-d.pred <- data.frame(
-    log_pop = log_pop.seq,
-    contact_high = 0
-)
-lambda.pred.l <- ensemble( m10.10 , m10.11 , m10.12 , data=d.pred )
-lambda.med <- apply( lambda.pred.l$link , 2 , median )
-lambda.PI <- apply( lambda.pred.l$link , 2 , PI )
-
-# plot again
-lines( log_pop.seq , lambda.med , lty=2 )
-shade( lambda.PI , log_pop.seq , col=col.alpha("black",0.1) )
 
 ## R code 10.49
-m10.10stan <- map2stan( m10.10 , iter=3000 , warmup=1000 , chains=4 )
-precis(m10.10stan)
 
 ## R code 10.50
-# construct centered predictor
-d$log_pop_c <- d$log_pop - mean(d$log_pop)
-
-# re-estimate
-m10.10stan.c <- map2stan(
-    alist(
-        total_tools ~ dpois( lambda ) ,
-        log(lambda) <- a + bp*log_pop_c + bc*contact_high +
-            bcp*log_pop_c*contact_high ,
-        a ~ dnorm(0,10) ,
-        bp ~ dnorm(0,1) ,
-        bc ~ dnorm(0,1) ,
-        bcp ~ dnorm(0,1)
-    ) ,
-    data=d , iter=3000 , warmup=1000 , chains=4 )
-precis(m10.10stan.c)
 
 ## R code 10.51
-num_days <- 30
-y <- rpois( num_days , 1.5 )
 
 ## R code 10.52
-num_weeks <- 4
-y_new <- rpois( num_weeks , 0.5*7 )
 
 ## R code 10.53
-y_all <- c( y , y_new )
-exposure <- c( rep(1,30) , rep(7,4) )
-monastery <- c( rep(0,30) , rep(1,4) )
-d <- data.frame( y=y_all , days=exposure , monastery=monastery )
 
 ## R code 10.54
-# compute the offset
-d$log_days <- log( d$days )
-
-# fit the model
-m10.15 <- map(
-    alist(
-        y ~ dpois( lambda ),
-        log(lambda) <- log_days + a + b*monastery,
-        a ~ dnorm(0,100),
-        b ~ dnorm(0,1)
-    ),
-    data=d )
 
 ## R code 10.55
-post <- extract.samples( m10.15 )
-lambda_old <- exp( post$a )
-lambda_new <- exp( post$a + post$b )
-precis( data.frame( lambda_old , lambda_new ) )
 
 ## R code 10.56
-# simulate career choices among 500 individuals
-N <- 500             # number of individuals
-income <- 1:3        # expected income of each career
-score <- 0.5*income  # scores for each career, based on income
-# next line converts scores to probabilities
-p <- softmax(score[1],score[2],score[3])
-
-# now simulate choice
-# outcome career holds event type values, not counts
-career <- rep(NA,N)  # empty vector of choices for each individual
-# sample chosen career for each individual
-for ( i in 1:N ) career[i] <- sample( 1:3 , size=1 , prob=p )
 
 ## R code 10.57
-# fit the model, using dcategorical and softmax link
-m10.16 <- map(
-    alist(
-        career ~ dcategorical( softmax(0,s2,s3) ),
-        s2 <- b*2,    # linear model for event type 2
-        s3 <- b*3,    # linear model for event type 3
-        b ~ dnorm(0,5)
-    ) ,
-    data=list(career=career) )
 
 ## R code 10.58
-N <- 100
-# simulate family incomes for each individual
-family_income <- runif(N)
-# assign a unique coefficient for each type of event
-b <- (1:-1)
-career <- rep(NA,N)  # empty vector of choices for each individual
-for ( i in 1:N ) {
-    score <- 0.5*(1:3) + b*family_income[i]
-    p <- softmax(score[1],score[2],score[3])
-    career[i] <- sample( 1:3 , size=1 , prob=p )
-}
-
-m10.17 <- map(
-    alist(
-        career ~ dcategorical( softmax(0,s2,s3) ),
-        s2 <- a2 + b2*family_income,
-        s3 <- a3 + b3*family_income,
-        c(a2,a3,b2,b3) ~ dnorm(0,5)
-    ) ,
-    data=list(career=career,family_income=family_income) )
 
 ## R code 10.59
-library(rethinking)
-data(UCBadmit)
-d <- UCBadmit
 
 ## R code 10.60
-# binomial model of overall admission probability
-m_binom <- map(
-    alist(
-        admit ~ dbinom(applications,p),
-        logit(p) <- a,
-        a ~ dnorm(0,100)
-    ),
-    data=d )
-
-# Poisson model of overall admission rate and rejection rate
-d$rej <- d$reject # 'reject' is a reserved word
-m_pois <- map2stan(
-    alist(
-        admit ~ dpois(lambda1),
-        rej ~ dpois(lambda2),
-        log(lambda1) <- a1,
-        log(lambda2) <- a2,
-        c(a1,a2) ~ dnorm(0,100)
-    ),
-    data=d , chains=3 , cores=3 )
 
 ## R code 10.61
-logistic(coef(m_binom))
 
 ## R code 10.62
-k <- as.numeric(coef(m_pois))
-exp(k[1])/(exp(k[1])+exp(k[2]))
 
 ## R code 10.63
-# simulate
-N <- 100
-x <- runif(N)
-y <- rgeom( N , prob=logistic( -1 + 2*x ) )
-
-# estimate
-m10.18 <- map(
-    alist(
-        y ~ dgeom( p ),
-        logit(p) <- a + b*x,
-        a ~ dnorm(0,10),
-        b ~ dnorm(0,1)
-    ),
-    data=list(y=y,x=x) )
-precis(m10.18)
 
 ## R code 11.1
 library(rethinking)
